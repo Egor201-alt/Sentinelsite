00a855cb0758cbbf8f121ede009cb4c82241f778
Egor201 <Egorsimikin934@gmail.com>
2026-01-04 18:03:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'; // <--- 1. Импортируй плагин
 
 export default defineConfig({
-  plugins: [vue()], // <--- 2. Добавь его в массив плагинов
+    plugins: [vue()], // <--- 2. Добавь его в массив плагинов
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        profile: resolve(__dirname, 'profile.html'), // Добавь эту строку для каждого нового html файла
+      },
+    },
+  },
 });
